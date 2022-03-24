b1984aa66370ce15f21ca41fdaca9c619c0e9aac
monellz <rxzhong@outlook.com>
2022-03-24 15:05:55 +0800

    add classof for ast

--- include/puzzle/frontend/ast.h
+++ include/puzzle/frontend/ast.h
@@ -10,16 +10,19 @@ struct Expr {
 };
 
 struct Binary : Expr {
+  static bool classof(Expr *p) { return p->kind >= Expr::kAdd && p->kind <= Expr::kOr; }
   Expr *lhs;
   Expr *rhs;
 };
 
 struct Access : Expr {
+  static bool classof(Expr *p) { return p->kind == Expr::kAccess; }
   std::string_view ident;
   std::vector<int> dims;
 };
 
 struct FloatLit : Expr {
+  static bool classof(Expr *p) { return p->kind == Expr::kFloatLit; }
   double val;
   static FloatLit ZERO;
 };
@@ -29,18 +32,21 @@ struct Stmt {
 };
 
 struct Assign : public Stmt {
+  static bool classof(Stmt *p) { return p->kind == Stmt::kAssign; }
   std::string_view ident;
   std::vector<int> index;
   Expr *rhs;
 };
 
 struct If : public Stmt {
+  static bool classof(Stmt *p) { return p->kind == Stmt::kIf; }
   Expr *cond;
   Stmt *on_true;
   Stmt *on_false;  // nullable
 };
 
 struct Block : public Stmt {
+  static bool classof(Stmt *p) { return p->kind == Stmt::kBlock; }
   std::vector<Stmt *> stmts;
 };
 
